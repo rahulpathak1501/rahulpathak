6ccba1b969baadfaa3f6dd14f20e80e4c73079e7
rahulpathak1501 <rahul150101130015@gmail.com>
2024-09-03 19:29:55 +0530

    New update branch (#4)
    * first time updating to origin/new-update-branch
    
    * code quality improvement and added some new css.
    
    * Resume link is created

--- src/Utills/Firebase.jsx
+++ src/Utills/Firebase.jsx
@@ -1,6 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
@@ -16,5 +17,6 @@ const db = getFirestore(app);
 
 const auth = getAuth(app);
 const provider = new GoogleAuthProvider();
+const storage = getStorage(app);
 
-export { auth, provider, app, db };
+export { auth, provider, app, db, storage };
