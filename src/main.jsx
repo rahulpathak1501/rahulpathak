741e446b3d2f76bdc40e83cf6c32c1bec68715da
rahulpathak1501 <rahul150101130015@gmail.com>
2024-09-13 23:24:42 +0530

    New design1 (#11)
    * first time updating to origin/new-update-branch
    
    * code quality improvement and added some new css.
    
    * Resume link is created
    
    * changes in packege.json
    
    * few small changes
    
    * few changes in navbar
    
    * Total design changes no feature change
    
    * rahulpathak 2.0 changes in css
    
    * rebuild
    
    * rebuild

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "../src/components/App";
 import "./index.css";
+import App from "../src/components/App";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
